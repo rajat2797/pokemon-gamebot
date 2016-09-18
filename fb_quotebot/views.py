72180c20b6d756d21b0316253af742c81fa5f4e8
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-18 12:11:24 +0530

    foo

--- fb_quotebot/views.py
+++ fb_quotebot/views.py
@@ -198,6 +198,8 @@ def post_facebook_message(fbid,message_text):
 	response_msg = json.dumps(response_msg_quickreply)
 	response_msg_img=json.dumps(response_msg_image)
 	response_msg_scoreit=json.dumps(response_msg_score)
+	if response_msg == output_text['answer'][0]:
+		logg("!!!!!!!!!!CORRECT!!!!!!!!!!!!!!!!!!",symbol='!!!')
 	requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg_img)
 	requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg_scoreit)
 	requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
