b4d32b8632866d3cf28aaffbe89a9e5d766bc6f6
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-18 11:01:43 +0530

    foo

--- fb_quotebot/urls.py
+++ fb_quotebot/urls.py
@@ -1,7 +1,18 @@
-from django.conf.urls import include, url
-urlpatterns = []
-from .views import MyQuoteBotView
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-urlpatterns = [
-                  url(r'^foo/?$', MyQuoteBotView.as_view()) 
-               ]
+from chatbot.views import MyChatBotView, index
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'hellobot.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', index),
+    #url(r'', 'chatbot.views.index'),
+    url(r'^facebook_auth/?$', MyChatBotView.as_view()),
+
+
+)
